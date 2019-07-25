b0a3ec6bda13279db908ef675e679efad9a3d41e
Kamalpreet Singh <kamsin@microsoft.com>
2019-07-24 18:24:13 -0700

    Added an example home page

---
+++ lib/MainScreenWidgets/HomeWidget.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import 'SearchWidget.dart';
+
+class HomePageWidget extends StatefulWidget {
+  HomePageWidget({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _HomePageWidgetState createState() => _HomePageWidgetState();
+}
+
+class _HomePageWidgetState extends State<HomePageWidget> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: new EcommerceTwoPage(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class MyListView extends StatelessWidget {
+  int _counter;
+  MyListView(this._counter);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'You have pushed the button this many times:',
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.display1,
+          ),
+        ],
+      ),
+    );
+  }
+}
